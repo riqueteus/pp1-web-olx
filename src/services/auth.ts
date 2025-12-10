7bd203e6e2cc7e72261829e6798410a634e10c8c
Mateus Henrique <mateus.santos@bemobi.com>
2025-12-10 16:04:37 -0300

    update

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+const API_BASE_URL = 'http://167.71.123.166:8080'
 
 export type RegisterVendorPayload = {
   nome: string
